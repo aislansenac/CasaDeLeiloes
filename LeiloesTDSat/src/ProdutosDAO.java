d353d0688dba1b88fcae1c08730599eb5519fe81
aislansenac <aislan.senacrs@gmail.com>
2025-01-09 14:45:38 -0300

    Botão Salvar produtos finalizado

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -1,19 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
     
@@ -23,10 +12,18 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        String sql = "INSERT INTO produtos(nome, valor, status) VALUES (?, ?, ?)";
         
-        
-        //conn = new conectaDAO().connectDB();
-        
+        try{
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.execute();
+        }catch(SQLException e){
+            System.out.println("Erro ao inserir produto: " + e.getMessage());
+        }
         
     }
     
